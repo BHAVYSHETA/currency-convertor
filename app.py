671a752b9466ff90dc4337481fa094b2a58eed32
BHAVY <bhavysheta1@gmail.com>
2025-07-11 18:22:07 +0530

    update app.py

--- app.py
+++ app.py
@@ -67,3 +67,8 @@ def logout():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("login.html"), 404
